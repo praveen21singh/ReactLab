5d2116b41420724c1420ba1e84223c8192e70581
PRAVEEN KUMAR SINGH <upl.praveensingh@gmail.com>
2023-10-09 14:18:46 +0530

    commit on 09 Oct 2023

--- 02tailwindreact/src/App.jsx
+++ 02tailwindreact/src/App.jsx
@@ -2,32 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
+let myObj = {
+  username:'Praveen',
+  department:'IT',
+  country:'India',
+  countryCode:'IN(+91)'
+}
 
+let myArray = [1,2,3,4];
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1 className='bg-yellow-400 p-10 rounded-2xl text-sky-900 mb-8'>Welcome to Tailwind first project</h1>
+    <Card session='codewithreact' author='praveen'  MyArr={myObj} OtherArray={myArray}/>
+    <Card session='codewithpraveen' author='KS Praveen' BtnAction='Purchase it'/>
+
+
     </>
   )
 }
